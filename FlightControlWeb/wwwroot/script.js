c438a5948745576e389df3e43634d36482300597
מאי בכר <mayb1995@gmail.com>
2020-05-19 14:45:48 +0300

    tables

--- FlightControlWeb/wwwroot/script.js
+++ FlightControlWeb/wwwroot/script.js
@@ -4,10 +4,10 @@ All scripts of the index.html, We added JQUERY to it
 */
 
 $(document).ready(function () {
-    var map = L.map('map').setView([0, 0], 1);
+    let map = L.map('map').setView([0, 0], 1);
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=LjrLBX4zwd2F8ebL9DTU', {
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     }).addTo(map);
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+    let marker = L.marker([51.5, -0.09]).addTo(map);
 
 });
